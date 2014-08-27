c7da11b7ee367620b4f22c3eccc13ce6f9c1df23
Martin Fenner <mfenner@plos.org>
2014-08-27 14:03:23 +0200

    add group attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,7 @@
 default['capistrano']['application'] = "app"
 default['capistrano']['rails_env'] = "production"
 default['capistrano']['deploy_user'] = "vagrant"
+default['capistrano']['group'] = "www-data"
 
 default['capistrano']['database'] = "#{node['capistrano']['application']}_#{node['capistrano']['rails_env']}"
 default['capistrano']['db_user'] = "root"
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "mfenner@plos.org"
 license           "Apache 2.0"
 description       "Rails basic configuration"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.1.0"
+version           "0.1.1"
 depends           "mysql"
 depends           "database"
 depends           "passenger_nginx", "~> 0.1.0"
