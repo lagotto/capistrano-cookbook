34f63d93d012747eeef53caaa170a0d7510cb2f1
Martin Fenner <mfenner@plos.org>
2014-08-28 23:51:24 +0200

    copy database file

--- definitions/db_app.rb
+++ definitions/db_app.rb
@@ -11,7 +11,7 @@ define :db_app, :template => 'database.yml.erb', :local => false, :enable => tru
       cwd "/var/www/#{node['capistrano']['application']}/current"
     end
   else
-    # create database.yml, symlink it, and setup database
+    # create database.yml, copy it, and setup database
 
     template filename do
       source params[:template]
@@ -27,7 +27,16 @@ define :db_app, :template => 'database.yml.erb', :local => false, :enable => tru
       action :create
     end
 
-    bash "ln -fs /var/www/#{node['capistrano']['application']}/shared/config/database.yml config/database.yml" do
+    # copy database file
+    remote_file "Copy database.yml" do
+      path "/var/www/#{node['capistrano']['application']}/current/config/database.yml"
+      source "file:///var/www/#{node['capistrano']['application']}/shared/config/database.yml"
+      owner node['capistrano']['deploy_user']
+      group node['capistrano']['group']
+      mode 0644
+    end
+
+    bash "cp /var/www/#{node['capistrano']['application']}/shared/config/database.yml config/database.yml" do
       user node['capistrano']['deploy_user']
       cwd "/var/www/#{node['capistrano']['application']}/current"
     end
