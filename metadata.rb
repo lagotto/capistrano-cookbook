33c4c68a7c03b235136d143bc262d7cde7cff9b0
Martin Fenner <mfenner@plos.org>
2014-09-01 02:26:12 +0200

    renamed file to template

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "mfenner@plos.org"
 license           "Apache 2.0"
 description       "Capistramo configuration"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.5.0"
+version           "0.5.1"
 depends           "ruby", "~> 0.2.0"
 
 %w{ ubuntu }.each do |platform|
