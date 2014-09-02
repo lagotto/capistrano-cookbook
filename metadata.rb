f2e224ed5cb7368f54707004aeb45e503564a41a
Martin Fenner <mfenner@plos.org>
2014-09-03 00:22:51 +0200

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,8 @@ maintainer_email  "mfenner@plos.org"
 license           "Apache 2.0"
 description       "Capistramo configuration"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.6.0"
-depends           "ruby", "~> 0.3.2"
+version           "0.6.1"
+depends           "ruby", "~> 0.3.0"
 
 %w{ ubuntu }.each do |platform|
   supports platform
