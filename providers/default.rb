fe9666e4d09e9d132d7261c68dcf4911dfd99466
Martin Fenner <mfenner@plos.org>
2014-09-03 13:45:44 +0200

    correctly reset bundler after using production environment

--- providers/default.rb
+++ providers/default.rb
@@ -40,7 +40,7 @@ action :bundle_install do
     user new_resource.user
     cwd "/var/www/#{new_resource.name}/current"
     if new_resource.rails_env == "development"
-      command "bundle install --path vendor/bundle --no-deployment"
+      command "bundle config --path vendor/bundle --delete without --no-deployment && bundle install"
     else
       command "bundle install --path vendor/bundle --deployment --without development test"
     end
