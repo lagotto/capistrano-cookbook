c6c65ed388e9e040b386bec83f95c47fadde737d
Martin Fenner <mfenner@plos.org>
2014-10-20 12:17:14 +0200

    use ENV variable DEPLOY_USER

--- resources/config.rb
+++ resources/config.rb
@@ -2,5 +2,5 @@ actions :load, :copy
 default_action :load
 
 attribute :name, :kind_of => String, :name_attribute => true
-attribute :user, :kind_of => String, :default => ENV['USER']
-attribute :group, :kind_of => String, :default => ENV['GROUP']
+attribute :user, :kind_of => String, :default => ENV['DEPLOY_USER']
+attribute :group, :kind_of => String, :default => ENV['DEPLOY_GROUP']
--- resources/default.rb
+++ resources/default.rb
@@ -2,6 +2,6 @@ actions :deploy, :config, :bundle_install, :precompile_assets, :migrate, :restar
 default_action :deploy
 
 attribute :name, :kind_of => String, :name_attribute => true
-attribute :user, :kind_of => String, :default => ENV['USER']
-attribute :group, :kind_of => String, :default => ENV['GROUP']
+attribute :user, :kind_of => String, :default => ENV['DEPLOY_USER']
+attribute :group, :kind_of => String, :default => ENV['DEPLOY_GROUP']
 attribute :rails_env, :kind_of => String, :default => ENV['RAILS_ENV']
