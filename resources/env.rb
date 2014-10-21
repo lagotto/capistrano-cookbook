a888dbcef2d0e4993f722b14b1a8b67c0b717188
Martin Fenner <mfenner@plos.org>
2014-10-21 15:02:23 +0200

    use separate cookbook for dotenv

--- resources/env.rb
+++
@@ -1,6 +0,0 @@
-actions :load, :copy
-default_action :load
-
-attribute :name, :kind_of => String, :name_attribute => true
-attribute :user, :kind_of => String, :default => ENV['DEPLOY_USER']
-attribute :group, :kind_of => String, :default => ENV['DEPLOY_GROUP']
--- resources/template.rb
+++ resources/template.rb
@@ -4,6 +4,6 @@ default_action :create
 attribute :name, :kind_of => String, :name_attribute => true
 attribute :application, :kind_of => String, :default => "app"
 attribute :source, :kind_of => String, :default => ""
-attribute :user, :kind_of => String, :default => ENV['DEPLOY_USER']
-attribute :group, :kind_of => String, :default => ENV['DEPLOY_GROUP']
+attribute :user, :kind_of => String, :default => node['ruby']['deploy_user']
+attribute :group, :kind_of => String, :default => node['ruby']['deploy_group']
 attribute :params, :kind_of => Hash, :default => {}
