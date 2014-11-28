5ce6b9530228b42f5e8ad4eb4ca12270c0af8244
Martin Fenner <mfenner@plos.org>
2014-11-28 19:08:20 +0100

    added build_ember action

--- providers/default.rb
+++ providers/default.rb
@@ -67,6 +67,25 @@ action :precompile_assets do
   end
 end
 
+action :ember_build do
+  run_context.include_recipe 'ruby'
+
+  # provide Rakefile if it doesn't exist, e.g. during testing
+  cookbook_file "Rakefile" do
+    path "/var/www/#{new_resource.name}/current/Rakefile"
+    owner new_resource.user
+    group new_resource.group
+    cookbook "capistrano"
+    action :create_if_missing
+  end
+
+  execute "bundle exec rake ember:build" do
+    user new_resource.user
+    environment 'RAILS_ENV' => new_resource.rails_env
+    cwd "/var/www/#{new_resource.name}/current"
+  end
+end
+
 action :migrate do
   run_context.include_recipe 'ruby'
 
