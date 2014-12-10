13bc04a0c15385c8607e3bbc6c66df64577cdbdc
Martin Fenner <mfenner@plos.org>
2014-12-10 23:46:09 +0100

    use template user home folder for nom config

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,3 @@
 default['ruby']['deploy_user'] = "vagrant"
 default['ruby']['deploy_group'] = "vagrant"
 default['ruby']['rails_env'] = "development"
-default['npm']['npm_config_prefix'] = "/home/#{node['ruby']['deploy_user']}/npm-config"
--- providers/default.rb
+++ providers/default.rb
@@ -62,7 +62,7 @@ action :npm_install do
       user new_resource.user
       creates "/home/#{new_resource.user}/npm-global/lib/node_modules/#{pkg}/"
       action :run
-      environment ({ 'NPM_CONFIG_PREFIX' => new_resource.npm_config_prefix })
+      environment ({ 'NPM_CONFIG_PREFIX' => "/home/#{new_resource.user}/npm-config" })
     end
   end
 end
@@ -82,7 +82,7 @@ action :bower_install do
   execute "bundle exec rake bower:install:deployment" do
     user new_resource.user
     environment ({ 'RAILS_ENV' => new_resource.rails_env,
-                   'NPM_CONFIG_PREFIX' => new_resource.npm_config_prefix })
+                   'NPM_CONFIG_PREFIX' => "/home/#{new_resource.user}/npm-config" })
     cwd "/var/www/#{new_resource.name}/current"
   end
 end
