3bdef27cc6101f07a86b006bba3bb32f73749994
Martin Fenner <mfenner@plos.org>
2015-07-02 23:49:32 -0700

    install nodejs via ruby

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,8 @@ maintainer_email  "mfenner@plos.org"
 license           "Apache 2.0"
 description       "Capistramo configuration"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.10.0"
-depends           "ruby", "~> 0.6.0"
+version           "0.10.1"
+depends           "ruby", "~> 0.7.0"
 depends           "consul"
 
 %w{ ubuntu }.each do |platform|
