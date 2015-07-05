58f8bce3b67b02bacf838b8a4197c1d3404f5c10
Martin Fenner <mfenner@plos.org>
2015-07-05 22:17:15 +0200

    use nodejs cookbook

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,5 +2,7 @@ default['ruby']['deploy_user'] = "vagrant"
 default['ruby']['deploy_group'] = "vagrant"
 default['ruby']['rails_env'] = "development"
 
+default['nodejs']['repo'] = 'https://deb.nodesource.com/node_0.12'
+
 default['consul']['servers'] = ENV['CONSUL_SERVERS'] || [ENV['HOSTNAME']]
 default['consul']['service_mode'] = 'cluster'
