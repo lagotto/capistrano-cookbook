643868b3ad167ff5e48a39560f5e1db25a71155d
Martin Fenner <martin.fenner@datacite.org>
2015-09-02 14:33:14 +0200

    check for npm configuration files

--- metadata.rb
+++ metadata.rb
@@ -4,13 +4,12 @@ maintainer_email  "martin.fenner@datacite.org"
 license           "Apache 2.0"
 description       "Capistramo configuration"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.10.15"
+version           "0.10.16"
 
 # opscode cookbooks
 depends           "apt"
 depends           "nodejs"
 depends           "consul"
-depends           "remote_syslog2"
 
 # our own cookbooks
 depends           "ruby", "~> 0.7.0"
